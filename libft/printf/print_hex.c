d0653ad1ac8c778fbd61f95c232048822e796dcd
Chleo PROESCHEL <cproesch@e2r9p7.clusters.42paris.fr>
2022-02-01 16:27:35 +0100

    updated printf -deleted 'if unsigned int < 0'

--- libft/printf/print_hex.c
+++ libft/printf/print_hex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_hex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 14:31:45 by avan-bre          #+#    #+#             */
-/*   Updated: 2021/08/09 17:36:27 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/02/01 16:25:51 by cproesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 int	print_hex(unsigned int number, int count)
 {
-	if (number < 0)
-	{
-		number = number * -1;
-		ft_putchar_fd('-', 1);
-		count++;
-	}
 	if (number < 10 )
 	{
 		ft_putchar_fd(number + 48, 1);
--- parser/parser.c
+++ parser/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avan-bre <avan-bre@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cproesch <cproesch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 18:55:08 by cproesch          #+#    #+#             */
-/*   Updated: 2022/02/01 12:48:25 by avan-bre         ###   ########.fr       */
+/*   Updated: 2022/02/01 15:43:30 by cproesch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,16 @@ int	review_operators(char **token)
 		if ((token[i][0] == '|') || (token[i][0] == '>')
 		|| (token[i][0] == '<'))
 		{
-			if (ft_strncmp(token[i], "|\0", 2) && ft_strncmp(token[i], "<\0", 2)
+			if ((ft_strncmp(token[i], "|\0", 2) 
+				&& ft_strncmp(token[i], "<\0", 2)
 				&& ft_strncmp(token[i], ">\0", 2)
 				&& ft_strncmp(token[i], ">>\0", 3)
 				&& ft_strncmp(token[i], "<<\0", 3)
 				&& ft_strncmp(token[i], ">|\0", 3))
-				return (ft_error("Syntax error"));
-			else if (!token[i + 1])
-				return (ft_error("Syntax error"));
-			else if (((i - 1) > -1) && ((token[i - 1][0] == '>') || (token[i - 1][0] == '<')))
+				|| (!token[i + 1])
+				|| (((i - 1) > -1) && ((token[i - 1][0] == '>')
+				|| (token[i - 1][0] == '<')))
+				|| ((token[i][0] == '|') && (token[i - 1][0] == '|')))
 				return (ft_error("Syntax error"));
 		}
 		i++;
